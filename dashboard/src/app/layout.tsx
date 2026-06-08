bfe332edbfd6bc0768826140f8be2683a836d9e9
it-operations-tarro <jack.banua@wondersco.com>
2026-06-08 12:57:01 -0400

    Apply date filter on the browse log

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
-      <body className="min-h-screen flex flex-col">{children}</body>
+      <body className="min-h-screen">{children}</body>
     </html>
   );
 }
